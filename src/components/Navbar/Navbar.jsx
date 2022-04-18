cd24fd39a73f08f34e11ac390bc9884c198b824d
Hector Eddin Acosta Najera <hector889ju@gmail.com>
2022-04-18 16:17:27 -0600

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+import './Navbar.css';
+
+const Navbar = () => (
+  <div>
+    Navbar
+  </div>
+);
+
+export default Navbar;
